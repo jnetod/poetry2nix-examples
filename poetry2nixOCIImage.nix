162685abff66320a92f95d33f324f190e4d3a126
J. Neto <xablau@tuta.io>
2021-02-04 17:39:29 -0300

    oci image crossbuild macos -> linux

--- poetry2nixOCIImage.nix
+++ poetry2nixOCIImage.nix
@@ -1,6 +1,6 @@
 { pkgs ?  import <nixpkgs> {} }:
 let
-    poetry2nixOCI = import ./poetry2nix.nix { inherit pkgs; };
+    poetry2nixOCI = import ./poetry2nix.nix { pkgs = pkgs.pkgsCross.gnu64; };
 in
     pkgs.dockerTools.buildLayeredImage {
     name = "numtild-dockertools-poetry2nix";
